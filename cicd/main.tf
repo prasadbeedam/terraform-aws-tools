b34ba7a532d1280df0e9ee654388886c69cf193c
prasad <prasad@gmail.com>
2024-07-08 12:06:06 +0530

    aws-tools

---
+++ cicd/main.tf
@@ -0,0 +1,59 @@
+module "jenkins" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-tf"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-04a8063bf88d0ec0c"] #replace your SG
+  subnet_id = "subnet-0534eac854c0e6cce" #replace your Subnet
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins.sh")
+  tags = {
+    Name = "jenkins-tf"
+  }
+}
+
+module "jenkins_agent" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-agent"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-04a8063bf88d0ec0c"] #replace your SG
+  subnet_id = "subnet-0534eac854c0e6cce" #replace your Subnet
+  ami = data.aws_ami.ami_info.id
+  user_data = file("agent.sh")
+  tags = {
+    Name = "jenkins-agent"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "jenkins"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins.public_ip
+      ]
+      allow_overwrite = true
+    },
+    {
+      name    = "jenkins-agent"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    }
+  ]
+
+}
+
