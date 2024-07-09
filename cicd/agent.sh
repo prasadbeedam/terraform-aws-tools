f50407d0da4b8109e32e758054a02e8480bca011
prasad <prasad@gmail.com>
2024-07-09 21:36:43 +0530

    tools

--- cicd/agent.sh
+++ cicd/agent.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 yum install fontconfig java-17-openjdk -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
